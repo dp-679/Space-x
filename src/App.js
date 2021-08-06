39a3dbe351c4354ab39a93618b2c877fc0d8b345
prasad <prasadthisis006@gmail.com>
2021-08-06 17:38:04 +0530

    dispaly launch on route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header";
 import { LaunchList } from "./components/LaunchList/LaunchList";
-import { LaunchView } from "./components/LaunchView/Launchview";
+import   LaunchView  from "./components/LaunchView/Launchview";
 import {
   BrowserRouter as Router,
   Switch,
--- src/components/LaunchView/Launchview.js
+++ src/components/LaunchView/Launchview.js
@@ -1,12 +1,52 @@
 import React from "react";
+import axios from "axios";
+import { withRouter } from 'react-router';
+import "./style.css"
 
-export class LaunchView extends React.Component{
-    render(){
-        return(
-            <div>
-                launch view
+export class LaunchView extends React.Component {
+
+    state = {
+        launch: {}
+    }
+
+    componentDidMount = () => {
+        const flightNo = this.props.match.params.flight_number
+        this.getLaunch(flightNo)
+    }
+
+    getLaunch = (flightNo) => {
+        axios.get("https://api.spacexdata.com/v3/launches/" + flightNo).then((response) => {
+            this.setState(
+                { launch: response.data }
+            )
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+
+    launchAttribute = (title,key) => {
+        const value = this.state.launch[key]
+        return (
+            <div className="attribute">
+                <h1>{title}</h1>
+                <h1>{value}</h1>
             </div>
         )
-        
+    }
+
+    render() {
+        console.log(this.state.launch)
+        return (
+            <div className="launch-view">
+                {this.launchAttribute("Mission Name","mission_name")}
+                {this.launchAttribute("Flight Number","flight_number")}
+                {this.launchAttribute("Launch Date","launch_date_local")}
+                <p>{this.state.launch.details}</p>
+            </div>
+        )
+
     }
 }
+
+export default withRouter(LaunchView)
